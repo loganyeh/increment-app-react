3167e3baed3da60af8d32d973cf526f2ac79d2bd
loganyeh <yehlogan@gmail.com>
2025-11-28 18:15:58 -0500

    css styling done

---
+++ src/components/CounterApp.jsx
@@ -0,0 +1,23 @@
+import { useState, useRef, useEffect, useContext } from "react";
+import { MyContext } from "./MyContext";
+
+function CounterApp(){
+    // const {} = useContext(MyContext);
+
+    return(
+        <>
+            <div className="h-screen w-screen flex justify-center items-center border-2 border-red-600">
+                <div className="h-1/2 w-1/2 border-2 border-blue-600 rounded-3xl">
+                    <div className="h-2/3 w-full flex flex-row text-9xl">
+                        <div className="h-full w-1/3 flex justify-center items-center hover:bg-gray-50 active:bg-gray-200 cursor-pointer rounded-3xl">-</div>
+                        <span className="h-full w-1/3 flex justify-center items-center hover:bg-gray-50 active:bg-gray-200 cursor-pointer rounded-3xl">1</span>
+                        <div className="h-full w-1/3 flex justify-center items-center hover:bg-gray-50 active:bg-gray-200 cursor-pointer rounded-3xl">+</div>
+                    </div>
+                    <div className="h-1/3 w-full flex justify-center items-center text-7xl hover:bg-gray-50 active:bg-gray-200 cursor-pointer rounded-3xl">Reset</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CounterApp;
